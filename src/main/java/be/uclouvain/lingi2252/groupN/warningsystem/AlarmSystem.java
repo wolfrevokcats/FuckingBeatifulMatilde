9952807ed7c0c01da6e5d8ce8325b24438fd3534
wolfrevokcats <boschierom.mb@gmail.com>
2018-11-13 22:47:06 +0100

    Quentin please help me, class AlarmSystem, method emergencyCall

--- src/main/java/be/uclouvain/lingi2252/groupN/warningsystem/AlarmSystem.java
+++ src/main/java/be/uclouvain/lingi2252/groupN/warningsystem/AlarmSystem.java
@@ -5,27 +5,34 @@ import be.uclouvain.lingi2252.groupN.Room;
 import be.uclouvain.lingi2252.groupN.signals.Motion;
 import be.uclouvain.lingi2252.groupN.signals.Signal;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Arrays;
+import java.util.ArrayList;
 
 public class AlarmSystem {
 
     //fields
     protected List<CommunicationHub> hubs;
-    private String emergencyNumber;
-    private String emergencyPerson;
     private Boolean status;
-
+    // <message,contact numbers>
+    private Map<String ,List<String>> contacts;
 
     public AlarmSystem(List<CommunicationHub> hubs) {
         this.hubs = hubs;
-        emergencyNumber = "112";
-        emergencyPerson = "[Adalberto]";
+        this.contacts = new HashMap<>();
+        this.contacts.put("FALL",new ArrayList<>(Arrays.asList("118","Adalberto")));
+        this.contacts.put("FIRE",new ArrayList<>(Arrays.asList("115","Natalie")));
+        this.contacts.put("BREAK-IN",new ArrayList<>(Arrays.asList("112","Julie")));
     }
 
     public void compute(Signal signal, Room room) {
         if (signal instanceof Motion && signal.extract().equals("FALL")) {
             emergencyCall("Somebody fell in [" + room.getName() + "]");
+
         }
+
     }
 
     public void ring(Room room, String issue) {
@@ -34,7 +41,7 @@ public class AlarmSystem {
     }
 
     public void emergencyCall(String message) {
-        System.out.println("Calling " + emergencyNumber + " with this message \"" + message + "\"");
+        System.out.println("Calling " + this.contacts.get(message) + " with this message \"" + message + "\"");
         System.out.println("Calling emergency contact " + emergencyPerson);
     }
 
@@ -42,8 +49,8 @@ public class AlarmSystem {
         this.emergencyNumber = emergencyNumber;
     }
 
-    public void setEngaged(Boolean status){
-        this.status = status;
+    public void setEngaged(Boolean flag){
+        this.status = flag;
         System.out.println("Alarm status: " + (status ? "armed" : "disarmed"));
     }
 
