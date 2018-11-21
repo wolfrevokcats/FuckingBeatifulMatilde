0fd886237da73ddfa553989002ecda27d33c43be
wolfrevokcats <boschierom.mb@gmail.com>
2018-11-21 16:29:52 +0100

    making main sure again

--- src/main/java/be/uclouvain/lingi2252/groupN/Main.java
+++ src/main/java/be/uclouvain/lingi2252/groupN/Main.java
@@ -5,23 +5,36 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
 
         System.out.println("--- Choose a scenario to run ---");
-        System.out.println(" Press \n 1: Scenario1 \n 2: Scenario2\n 3: Scenario3");
-        int input = (new Scanner(System.in)).nextInt();
-        switch(input){
-            case 1:
-                System.out.println(" --- First Scenario Implemented ---");
-                Scenario.scenario1();
-                break;
+        System.out.println(" Press \n 0: Quit \n 1: Scenario1 \n 2: Scenario2\n 3: Scenario3");
+        boolean flag = true;
+        while(flag){
+            try{
+                int input = (new Scanner(System.in)).nextInt();
+                switch(input){
+                    case 0:
+                        System.out.println("--- End of the demo ---");
+                        break;
+                    case 1:
+                        System.out.println("--- First Scenario Implemented ---");
+                        Scenario.scenario1();
+                        break;
 
-            case 2:
-                System.out.println("--- Second Scenario Implemented ---");
-                Scenario.scenario2();
-                break;
+                    case 2:
+                        System.out.println("--- Second Scenario Implemented ---");
+                        Scenario.scenario2();
+                        break;
 
-            case 3:
-                System.out.println("--- Third Scenario Implemented ---");
-                Scenario.scenario3();
-                break;
+                    case 3:
+                        System.out.println("--- Third Scenario Implemented ---");
+                        Scenario.scenario3();
+                        break;
+                }
+                flag = false;
+            }
+            catch(java.util.InputMismatchException e) {
+                System.out.println("Please enter one of the following options:" +
+                        "\n 1: Scenario1 \n 2: Scenario2 \n 3: Scenario3");
+            }
         }
 
     }
